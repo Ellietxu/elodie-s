40e8082da72428f496793454a0ceba8c0f010b4a
Elodie S <elodie.sevestre86@gmail.com>
2026-02-11 20:28:58 +0100

    exercice done

--- exercices_individuels/script.js
+++ exercices_individuels/script.js
@@ -1,13 +1,28 @@
+// function sayHello(firstName, hour) {
+//   if (hour >= 18) {
+//     let message = "Bonsoir " + firstName + " !";
+//     console.log(message);
+//   } else {
+//     let message = "Bonjour " + firstName + " !";
+//     console.log(message);
+//   }
+// }
+
+// sayHello("Beyonce", 11);
+// sayHello("Beyonce", 18);
+// sayHello("Beyonce", 17);
+
+let firstName = prompt("Quel est ton prénom?");
+let hour = prompt("Dans quelle heure somme-nous entre 0 et 24?");
+
 function sayHello(firstName, hour) {
   if (hour >= 18) {
     let message = "Bonsoir " + firstName + " !";
-    console.log(message);
+    document.querySelector("h1").innerText = message;
   } else {
     let message = "Bonjour " + firstName + " !";
-    console.log(message);
+    document.querySelector("h1").innerText = message;
   }
 }
 
-sayHello("Beyonce", 11);
-sayHello("Beyonce", 18);
-sayHello("Beyonce", 17);
+sayHello(firstName, hour);
