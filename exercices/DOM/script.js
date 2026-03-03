65cae0e2756e99270ef5ebcfba077693901c8f35
Elodie S <elodie.sevestre86@gmail.com>
2026-03-03 17:12:54 +0100

    ex DOM

---
+++ exercices/DOM/script.js
@@ -0,0 +1,17 @@
+// variables initialisées au chargement de la page
+const title = document.querySelector(".title1");
+const button = document.querySelector(".button1");
+const ul = document.getElementById("list1");
+
+button.addEventListener("click", () => {
+  //   console.log("Bouton cliqué");
+  title.textContent = "Piou piou";
+  title.style.color = "orange";
+  const addLi = document.createElement("li");
+  liQuatre.textContent = "poule";
+  ul.appendChild(addLi);
+  const li = document.querySelectorAll("li"); // placé ici pour prendre en compte les 3 éléments initiaux plus celui qu'on ajoute
+  li.forEach((maLigne) => {
+    maLigne.style.color = "blue";
+  });
+});
